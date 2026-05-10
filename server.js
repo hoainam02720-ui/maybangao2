a305fc668bbeba53391fa58c8b93ea4c7604b57d
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-10 19:04:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
 
-let trangThai = "OFF";
+let paid = false;
 
+// Trang chính
 app.get("/", (req, res) => {
-  res.send("Máy bán gạo đang chạy 😄");
+  res.send(paid ? "ON" : "OFF");
 });
 
+// Nhận webhook từ SePay
 app.post("/webhook", (req, res) => {
-
   console.log(req.body);
 
-  trangThai = "ON";
+  paid = true;
 
-  res.send("Đã nhận thanh toán");
+  setTimeout(() => {
+    paid = false;
+  }, 30000);
 
+  res.status(200).send("ok");
 });
 
-app.get("/status", (req, res) => {
+const PORT = process.env.PORT || 3000;
 
-  res.send(trangThai);
-
-  trangThai = "OFF";
-
-});
-
-app.listen(8080, () => {
-  console.log("Server chạy cổng 8080");
+app.listen(PORT, () => {
+  console.log("Server chạy");
 });
