1890b306f29cd12a5ccc18b07fc15e1fc7d29b0a
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-10 15:44:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server đang chạy cổng " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server chạy");
 });
