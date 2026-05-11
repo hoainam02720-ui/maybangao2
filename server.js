1a5898630e89161af2dc205df47eb0936a8cbcaa
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-11 17:20:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,9 @@
 const express=require("express")
 const app=express()
 
-app.use(express.json())
-
-let orders=[]
-
-let machine={
-lock:true
-}
+let locked=true
 
 app.get("/",(req,res)=>{
-
-let html=orders.map(o=>`
-<div style="border:3px solid yellow;margin:20px;padding:20px">
-
-<div style="font-size:50px;color:cyan">
-${o.type}
-</div>
-
-<div style="font-size:35px">
-${o.id}
-</div>
-
-<div style="font-size:35px;color:lime">
-${o.money}đ
-</div>
-
-</div>
-`).join("")
 
 res.send(`
 <body style="background:black;color:white">
@@ -36,55 +12,37 @@ res.send(`
 NAMRICE AUTO
 </div>
 
-<div style="font-size:55px;color:lime">
-${machine.lock?"ĐANG KHÓA":"ĐÃ MỞ KHÓA"}
+<div style="font-size:55px;color:${locked ? "lime" : "red"}">
+${locked ? "ĐANG KHÓA" : "ĐÃ MỞ KHÓA"}
 </div>
 
-${html}
+<div style="font-size:40px">
+MÁY MAY01
+</div>
+
+<div style="font-size:30px">
+185 TỔ 8 ẤP ĐÔNG THẠNH
+</div>
 
 </body>
 `)
-})
-
-app.get("/machine",(req,res)=>{
-
-res.json(machine)
 
 })
 
-app.post("/sepay",(req,res)=>{
+app.get("/picked",(req,res)=>{
 
-let data=req.body
+locked=false
 
-orders.unshift({
-type:"SEPAY",
-id:data.content || "KHACH",
-money:data.transferAmount || 0
+res.send("DA MO KHOA")
+
 })
 
-res.send("OK")
+app.get("/lock",(req,res)=>{
+
+locked=true
+
+res.send("DA KHOA")
+
 })
 
-app.post("/tiktok",(req,res)=>{
-
-let data=req.body
-
-machine.lock=true
-
-orders.unshift({
-type:"TIKTOK",
-id:data.order_id || "DONMOI",
-money:data.price || 0
-})
-
-res.send("OK")
-})
-
-app.post("/picked",(req,res)=>{
-
-machine.lock=false
-
-res.send("OPEN")
-})
-
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
