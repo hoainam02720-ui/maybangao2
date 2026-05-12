aab00bd3ddc72cd56e7b0a50937427d38d4d6f0a
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-12 18:16:12 +0700

    Implement order management routes
    Add routes to handle orders and place orders.

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ app.use(express.static(__dirname))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"))
 })
+let donHang = []
 
+app.get("/donhang", (req, res) => {
+res.json(donHang)
+})
+
+app.post("/datgao", express.json(), (req, res) => {
+
+donHang.push(req.body)
+
+res.json({
+ok: true
+})
+
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
