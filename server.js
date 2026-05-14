b604a070a2203abc1247f99fce234cf321346ee8
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-14 20:37:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,15 +14,41 @@ app.get("/donhang", (req, res) => {
 res.json(donHang)
 })
 
-app.post("/datgao", express.json(), (req, res) => {
+app.post("/sepay", express.json(), async (req, res) => {
 
-donHang.push(req.body)
+  const data = req.body
 
-res.json({
-ok: true
+  console.log(data)
+
+  const amount = Number(data.transferAmount || 0)
+
+  const content = String(data.content || "").toLowerCase()
+
+  if (
+    amount >= 2000 &&
+    content.includes("may gao st25 01")
+  ) {
+
+    try {
+
+      await fetch("http://192.168.1.14/mo")
+
+      console.log("DA MO MAY")
+
+    } catch (err) {
+
+      console.log(err)
+
+    }
+
+  }
+
+  res.json({
+    ok: true
+  })
+
 })
 
-})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
